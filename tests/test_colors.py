9ca6fc39e99d4f18a6fb8ac0654bcd678b725114
Jakub Rakoczy <kuba.rakoczy@gmail.com>
2017-09-22 14:34:52 +0200

    Remove tests clashing with the Nix pkgs installation process

--- tests/test_colors.py
+++ tests/test_colors.py
@@ -7,11 +7,6 @@ from pywal import colors
 class TestGenColors(unittest.TestCase):
     """Test the gen_colors functions."""
 
-    def test_gen_colors(self):
-        """> Generate a colorscheme."""
-        result = colors.get("tests/test_files/test.jpg")
-        self.assertEqual(len(result["colors"]["color0"]), 7)
-
     def test_gen_colors_fail(self):
         """> Generate a colorscheme and fail."""
         with self.assertRaises(SystemExit):
--- tests/test_image.py
+++ tests/test_image.py
@@ -6,15 +6,6 @@ from pywal import image
 
 class TestImage(unittest.TestCase):
     """Test image functions."""
-    def test_get_img(self):
-        """> Validate image file."""
-        result = image.get("tests/test_files/test.jpg")
-        self.assertIn("tests/test_files/test.jpg", result)
-
-    def test_get_img_dir(self):
-        """> Validate image directory."""
-        result = image.get("tests/test_files")
-        self.assertEqual(result.endswith((".jpg", ".png")), True)
 
     def test_get_img_fail(self):
         """> Validate image file. (fail)"""
